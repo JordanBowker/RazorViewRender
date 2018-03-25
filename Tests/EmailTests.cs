0bbf432de964318c8b4c7f475f3b501e42c98cd7
Jordan Bowker <jordanbowker90@gmail.com>
2018-03-25 21:24:27 +0100

    Removed the need for a webhost in the test suite

--- Tests/EmailTests.cs
+++ Tests/EmailTests.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.Extensions.PlatformAbstractions;
 using NUnit.Framework;
 using RazorViewRender.Domain.ViewRenderService;
 using RazorViewRender.WebApp.EmailTemplates.Models;
+using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -18,11 +24,44 @@ namespace RazorViewRender.Tests
 		[SetUp]
 		public void Setup()
 		{
-			var webHost = WebHost.CreateDefaultBuilder()
-								 .UseStartup<TestStartup>()
-								 .Build();
+			var services = new ServiceCollection();
 
-			_viewRenderingService = webHost.Services.GetService<IViewRenderingService>();
+			ConfigureDefaultServices(services);
+
+			var serviceProvider = services.BuildServiceProvider();
+			_viewRenderingService = serviceProvider.GetService<IViewRenderingService>();
+		}
+
+
+		private void ConfigureDefaultServices(IServiceCollection services)
+		{
+			services.AddSingleton(PlatformServices.Default.Application);
+
+			var appDirectory = Directory.GetCurrentDirectory();
+			services.AddSingleton<IHostingEnvironment>(new HostingEnvironment
+			{
+				WebRootFileProvider = new PhysicalFileProvider(appDirectory),
+				ApplicationName = "RazorViewRender.Tests"
+			});
+
+			var assembly = typeof(EmailModel).GetTypeInfo().Assembly;
+
+			//Create an EmbeddedFileProvider for that assembly
+			var embeddedFileProvider = new EmbeddedFileProvider(assembly, "RazorViewRender.WebApp");
+
+			//Add the file provider to the Razor view engine
+			services.Configure<RazorViewEngineOptions>(options =>
+			{
+				options.FileProviders.Clear();
+				options.FileProviders.Add(embeddedFileProvider);
+			});
+
+			services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+			services.AddSingleton<DiagnosticSource>(new DiagnosticListener("Microsoft.AspNetCore"));
+
+			services.AddLogging();
+			services.AddMvc();
+			services.AddTransient<IViewRenderingService, ViewRenderingService>();
 		}
 
 		[Test]
